87aaa6aba25790f4ae83a716ae1bfe0358e8a21f
Elmer <Brandon.Elmer@denvergov.org>
2020-11-10 19:01:57 -0700

    removed functions to module file

--- five crowns.py
+++ five crowns.py
@@ -1,71 +1,11 @@
 # The app will have 2 choices when launching:
 # ongoing competition or single play
 
-# capture the number of players
-def people():
-    users = (input("how many players? "))
-    while not users.isdigit():
-        users = (input("how many players? "))
-    return int(users)
-
-users = people()
-
-# capture each player's name
-def player_name():
-    for person in range(users):
-        name = input("player's name?  ")
-        players[name] = []
-
-rounds = range(3,14)
-
-# capture player scores
-def scores():
-    for round in rounds:
-        for player in players:
-            score = (input(f"{player.title()}'s round {round} score?  "))
-            while not score.isdigit():
-                score = (input(f"{player.title()}'s round {round} score?  "))
-            players[player].append(int(score))
-      
-        print(f"\nScores after round {round}")
-        for player, score in players.items():
-            print(f"{player.title()}: {sum(score)}")
-        print()
-
-# print scores by round
-def round_scores():
-    print()
-    print("Scores By Round")
-    print(players)
-
-# tell each player their final score
-def final_scores():
-    for player, score in players.items():
-        print(f"\n{player.title()}'s final score is {sum(score)}")
-
-# announce the winner
-def winner():
-    scores = []
-    for player, score in players.items():
-        scores.append(sum(score)) 
-    
-    scores.sort()
-    score_spread = scores[1] - scores[0]
-    
-    winner=min(players, key=lambda p:sum(players[p]))
-   
-    if score_spread == 0:
-        print("There is a tie.")
-    elif score_spread == 1:
-        print(f"\nThe winner is {winner.title()} by {score_spread} point!")
-    else:
-        print(f"\nThe winner is {winner.title()} by {score_spread} points!")
-
-    print()
-
-players = {}
+from five_crowns_functions import *
 
 game_type = input("ongoing competition or single play?  ")
+
+
 
 if game_type == 'single play':
     people()

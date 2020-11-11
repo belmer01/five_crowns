8ff0d29f654d95337e54b7d5260f6b129422c18c
Elmer <Brandon.Elmer@denvergov.org>
2020-11-10 19:05:24 -0700

    test

--- five crowns.py
+++ five crowns.py
@@ -1,11 +1,9 @@
 # The app will have 2 choices when launching:
 # ongoing competition or single play
 
-from five_crowns_functions import *
+from functions import *
 
 game_type = input("ongoing competition or single play?  ")
-
-
 
 if game_type == 'single play':
     people()
